317ef7a2bfd2c63ae8c87c71a3546d8efd4a7191
Azalea Colburn <elocolburn@comcast.net>
2024-08-17 13:44:05 -0700

    refactor: switch to macro-structure-based design

--- src/main.rs
+++ src/main.rs
@@ -1,104 +1,45 @@
-use std::path::{Path, PathBuf};
+use std::path::PathBuf;
 
-use clap::{Arg, ArgAction, Command};
+use clap::{Parser, Subcommand};
 use serde::Deserialize;
 
-static CONFIG_FILE_NAME: &str = "config.toml"
+static CONFIG_FILE_NAME: &str = "config.toml";
 
 #[derive(Deserialize)]
 struct Config {
     install_location: PathBuf,
 }
 
+#[derive(Parser, Debug)]
+#[clap(name = "alloy", author, version, about, long_about = None)]
+struct Args {
+    #[clap(subcommand)]
+    command: Command,
+}
+
+#[derive(Subcommand, Debug, Clone)]
+enum Command {
+    #[clap(name = "install", value_parser)]
+    Install { package: String },
+
+    #[clap(name = "update", value_parser)]
+    Update { package: String },
+
+    #[clap(name = "query", value_parser)]
+    Query { substring: String },
+
+    #[clap(name = "list", value_parser)]
+    List,
+}
+
 fn main() {
-    let matches = Command::new("alloy")
-        .about("Package Manager for Autodesk Fusion")
-        .version("0.0.1")
-        .subcommand_required(true)
-        .arg_required_else_help(true)
-        // Query subcommand
-        //
-        // Only a few of its arguments are implemented below.
-        .subcommand(
-            Command::new("query")
-                .short_flag('q')
-                .long_flag("query")
-                .about("Query the package database.")
-                .arg(
-                    Arg::new("local")
-                        .short('l')
-                        .long("local")
-                        .help("search locally installed plugins for matching strings")
-                        .action(ArgAction::Set)
-                        .num_args(1..),
-                ),
-        )
-        .subcommand(
-            Command::new("install")
-                .short_flag('i')
-                .long_flag("install")
-                .about("Install package."),
-        )
-        .subcommand(
-            Command::new("list")
-                .short_flag('l')
-                .long_flag("list")
-                .about("List locally installew packages."),
-        )
-        .subcommand(
-            Command::new("update")
-                .short_flag('u')
-                .long_flag("update")
-                .about("Update package version."),
-        )
-        .get_matches();
+    let cli = Args::parse();
+    println!("{:?}", cli);
 
-    match matches.subcommand() {
-        Some(("install", install_matches)) => {}
-        Some(("query", substring_matches)) => {}
-        Some(("list", list_matches)) => {}
-        Some(("update", update_matches)) => {}
-        None => {}
-        _ => {}
+    match cli.command {
+        Command::Install { package } => {}
+        Command::Update { package } => {}
+        Command::Query { substring } => {}
+        Command::List => {}
     }
-
-    // match matches.subcommand() {
-    //     Some(("sync", sync_matches)) => {
-    //         if sync_matches.contains_id("search") {
-    //             let packages: Vec<_> = sync_matches
-    //                 .get_many::<String>("search")
-    //                 .expect("contains_id")
-    //                 .map(|s| s.as_str())
-    //                 .collect();
-    //             let values = packages.join(", ");
-    //             println!("Searching for {values}...");
-    //             return;
-    //         }
-    //
-    //         let packages: Vec<_> = sync_matches
-    //             .get_many::<String>("package")
-    //             .expect("is present")
-    //             .map(|s| s.as_str())
-    //             .collect();
-    //         let values = packages.join(", ");
-    //
-    //         if sync_matches.get_flag("info") {
-    //             println!("Retrieving info for {values}...");
-    //         } else {
-    //             println!("Installing {values}...");
-    //         }
-    //     }
-    //     Some(("query", query_matches)) => {
-    //         if let Some(packages) = query_matches.get_many::<String>("info") {
-    //             let comma_sep = packages.map(|s| s.as_str()).collect::<Vec<_>>().join(", ");
-    //             println!("Retrieving info for {comma_sep}...");
-    //         } else if let Some(queries) = query_matches.get_many::<String>("search") {
-    //             let comma_sep = queries.map(|s| s.as_str()).collect::<Vec<_>>().join(", ");
-    //             println!("Searching Locally for {comma_sep}...");
-    //         } else {
-    //             println!("Displaying all locally installed packages...");
-    //         }
-    //     }
-    //     _ => unreachable!(), // If all subcommands are defined above, anything else is unreachable
-    // }
 }
